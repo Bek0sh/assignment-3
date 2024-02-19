70e9b83be7a25122e213196cd6251c95ceba409a
Bek0sh <bekarys.0804@bk.ru>
2024-02-19 19:15:21 +0600

    first commit

---
+++ services/contact/cmd/app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"architecture_go/pkg/store/postgres"
+	delivery_http "architecture_go/services/contact/internal/delivery/http"
+	postgres2 "architecture_go/services/contact/internal/repository/storage/postgres"
+	"architecture_go/services/contact/internal/useCase/contact"
+	"architecture_go/services/contact/internal/useCase/group"
+	"log"
+)
+
+func main() {
+	storage, err := postgres.New("localhost", "postgres", "1234", "postgres", 5432)
+	if err != nil {
+		log.Fatal("failed to init storage", err)
+	}
+	contactRepo := postgres2.NewContactRepository(storage)
+	contactUC := contact.NewContactUseCase(contactRepo)
+	groupUC := group.NewGroupUseCase(contactRepo, contactRepo)
+	delivery := delivery_http.NewContactHTTP(contactUC, groupUC)
+	delivery.Start(8000)
+}
---
+++ services/contact/internal/domain/contact/type.go
@@ -0,0 +1,40 @@
+package contact
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"regexp"
+)
+
+type Contact struct {
+	ID                                           uuid.UUID
+	firstName, lastName, middleName, phoneNumber string
+}
+
+func New(id uuid.UUID, firstName, lastName, middleName, phoneNumber string) (*Contact, error) {
+	contact := &Contact{
+		ID:         id,
+		firstName:  firstName,
+		lastName:   lastName,
+		middleName: middleName,
+	}
+	if err := contact.SetPhoneNumber(phoneNumber); err != nil {
+		return nil, err
+	}
+
+	return contact, nil
+}
+
+func (c *Contact) FullName() string {
+	return c.lastName + " " + c.firstName + " " + c.middleName
+}
+
+func (c *Contact) SetPhoneNumber(phoneNumber string) error {
+	const op = "internal.domain.SetPhoneNumber"
+	_, err := regexp.MatchString("^[0-9]+$", phoneNumber)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	c.phoneNumber = phoneNumber
+	return nil
+}
